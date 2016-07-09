68955086a4d335683e1b15a4381da3bec7a14a4a
inuscript <inuscript@gmail.com>
2016-07-09 11:48:43 +0900

    remove unused function

--- analyser.js
+++ analyser.js
@@ -1,15 +1,5 @@
 const { getTokenizer } = require('kuromojin')
 
-const getLattice = (text) => {
-  return getTokenizer().then(tokenizer => {
-    return { tokenizer: tokenizer, lattice: tokenizer.getLattice(text) }
-  })
-}
-const bestPath = (text) => {
-  return getLattice().then( ({tokenizer, lattice}) => {
-    return tokenizer.viterbi_searcher.search(lattice);
-  })
-} 
 const analyse = (text) => {
   return getTokenizer().then(tokenizer => {
     const lattice = tokenizer.getLattice(text) 
@@ -19,5 +9,5 @@ const analyse = (text) => {
 }
 
 module.exports = {
-  getLattice, bestPath, analyse
+  analyse
 }
